ccac0a7a3ccb0d3ae1c58a0d08918738e91e67d3
Gavi Teitz <gavi@mellanox.com>
2018-01-21 13:27:04 +0200

    Bluefield: Added support for verification of HW rules
    Added a new family of classes to the Bluefield test lib to represent
    offloaded HW rules, with each type of rule extending the base HwRule class,
    and overridding the rules identifying regex.
    
    Added an initial class for L2 forwarding rules.
    
    Added a method to the Bluefield test lib which dumps the HW rules of a
    given host for a given gvmi, and verifies that the dumped rules contain
    matches for each of a given set of HwRules.
    
    Added a prep script to the repository, which will fetch the pretty_dump
    repository used for the HW rule verification, and added the pretty_dump
    repository into the gitignore.
    
    Signed-off-by: Gavi Teitz <gavi@mellanox.com>

---
+++ prep.sh
@@ -0,0 +1,4 @@
+if [ ! -d "pretty-fs-dump" ]; then
+    echo "Fetching pretty_dump"
+    git clone https://github.com/roidayan/pretty-fs-dump.git
+fi
--- recipes/Bluefield/BluefieldTestlib.py
+++ recipes/Bluefield/BluefieldTestlib.py
@@ -4,6 +4,24 @@
 import sys
 sys.path.append('../ovs_offload')
 from Testlib import Testlib
+import re
+import logging
+from os import linesep
+
+class HwRule(object):
+    def __init__(self, description):
+        self.regex = r''
+        self.description = description
+
+    def matches(self, line):
+        return True if re.search(self.regex, line) else False
+
+class L2ForwardingRule(HwRule):
+    def __init__(self, description, srcMacAddress, dstMacAddress, forwardingDestination, ethType = '0x[0-9a-f]{4}', inPort = None):
+        super(L2ForwardingRule, self).__init__(description)
+        self.regex = r'eth\(src=%s,dst=%s\),eth_type\(%s\),.*action:%s' % (str(srcMacAddress).lower(), str(dstMacAddress).lower(), str(ethType), forwardingDestination.lower())
+        if inPort:
+            self.regex = r'in_port\(%s\),' % (inPort) + self.regex
 
 class BluefieldTestlib(Testlib):
     def syncTestModules(self, hostToTestModulesDict):
@@ -32,3 +50,25 @@ class BluefieldTestlib(Testlib):
         host.run(self._ctl.get_module(pingModule, options=options), timeout=pingTimeout, bg=runInBackground)
         if runInBackground:
             self._ctl.wait(backgroundDelay)
+
+    def verifyRulesAreInHw(self, host, gvmi, rules):
+        host.run('mlxdump -d ' + str(self._ctl.get_alias('mstDev')) + ' fsdump --no_zero 1 --type FT --gvmi ' + str(gvmi) + ' > /tmp/lnstHwDump')
+        prettyDumpResults = host.run('python pretty-fs-dump/pretty_dump.py -s /tmp/lnstHwDump -vvv').out().strip()
+        if len(prettyDumpResults) < 1 and len(rules) > 0:
+            self.custom(host, 'Verifying rules are in HW', err_msg = 'Didn\'t find any HW dump results')
+            return
+        prettyDumpResults = prettyDumpResults.split(linesep)
+
+        allRulesWereFound = True
+        for currentRule in rules:
+            matchingLineFound = False
+            for line in prettyDumpResults:
+                if currentRule.matches(line):
+                    matchingLineFound = True
+                    break
+            if not matchingLineFound:
+                allRulesWereFound = False
+                self.custom(host, 'Verifying rules are in HW', err_msg = 'Couldn\'t find a mathcing rule for: ' + str(currentRule.description))
+        if allRulesWereFound:
+            logging.debug('Verified the following rules are in HW: ' + linesep + linesep.join(['    ' + rule.description for rule in rules]))
+            self.custom(host, 'Verified rules are in HW')
